1642ad6520938d11069f24758e27da59b5174de9
Saul <saulcastel018@gmail.com>
2024-04-19 17:53:30 -0600

    Base project

---
+++ DB/database.sql
@@ -0,0 +1,32 @@
+CREATE TABLE profile_role (
+    id INTEGER PRIMARY KEY,
+    description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE profile_state (
+    id INTEGER PRIMARY KEY,
+    description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE profile (
+    email VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    state INTEGER DEFAULT 0 REFERENCES profile_state(id),
+    role INTEGER DEFAULT 1 REFERENCES profile_role(id)
+);
+
+CREATE TABLE payment_type (
+    id INTEGER PRIMARY KEY,
+    description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE payment (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    quantity INTEGER NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    payment_date DATE NOT NULL,
+    ptype INTEGER REFERENCES payment_type(id),
+    comments VARCHAR(255) NOT NULL
+);
---
+++ DB/inserts.sql
@@ -0,0 +1,4 @@
+INSERT INTO profile_role VALUES (0, 'administrador'), (1, 'usuario');
+INSERT INTO profile_state VALUES (0, 'no activo'), (1, 'activo');
+INSERT INTO payment_type VALUES (0, 'hora normal'), (1, 'bono'), (2, 'hora extra');
+INSERT INTO profile VALUES ('admin@e4cc.com', 'Saul', 'Castellanos', 'password', 1, 0);
